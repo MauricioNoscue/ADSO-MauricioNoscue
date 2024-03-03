ef5224fc5f1d73fdaf62ba925ebf8ed8c78e91d2
mauricio noscue <mauronoscue@gmail.com>
2024-03-03 14:14:01 -0500

    ejercicio1

---
+++ tecnico/2024/marzo/inversiones.py
@@ -0,0 +1,14 @@
+p1= float(input("ingrese la cantidad de la primera persona:"))
+p2 = float(input("Ingrese la cantidad de la segunda persona:"))
+p3 = float(input("Ingresa la cantidad de la tercera persona: "))
+
+cantidad_total = p1 + p2 +p3
+por1 =  (p1 / cantidad_total) * 100
+por2 = (p2 / cantidad_total) * 100
+por3 = (p3 / cantidad_total) * 100
+print ("El porcentaje que compró la primera persona fue del",por1,"%")
+print  ("El porcentaje que compró la segunda persona fue del ",por2, "%")
+print (" El porcentaje que compró la tercera persona fue del ", por3 ,"%.")
+ 
+ 
+ 
