66b5936e8129be690b910d9b178b86c996d6da08
mauricio noscue <mauronoscue@gmail.com>
2024-07-10 12:40:50 -0500

    arreglos

--- tecnico/javascript/arreglos/factorial/js/arreglos.js
+++ tecnico/javascript/arreglos/factorial/js/arreglos.js
@@ -1,11 +1,33 @@
 /**
  * Factorial
  * Autor: Mauricio Noscue 
- * Fehca: 08/07/2024
+ * Fecha: 08/07/2024
  */
 
+let arreglo = [];
+let iteracion;
+let factorial;
+let numero2;
+let resultaLista = "";
+
+numero2 = 5;
+factorial = 1;
+
+for(iteracion = 1; iteracion <= numero2; iteracion++){
+    factorial = factorial * iteracion;
+    arreglo.push({ numero: iteracion, factorial: factorial });
+}
+
+for (iteracion = 0; iteracion < arreglo.length; iteracion++) {
+    resultaLista += `<tr><td>${arreglo[iteracion].numero}</td><td>${arreglo[iteracion].factorial}</td></tr>`; 
+}
+
+document.getElementById('lista').innerHTML = resultaLista;
 
 
+
+
+/** 
 let arreglo = [];
 let iteracion;
 let factorial;
@@ -22,15 +44,14 @@ let resultaLista = ""
 
 
  for (iteracion = 0; iteracion < arreglo.length; iteracion++) {
-    resultaLista += `<th>`+arreglo[iteracion]+ `</th>`  ; 
+    resultaLista += <th>+arreglo[iteracion]+ </th>  ; 
 }
- document.getElementById(`lista-numeros`).innerHTML = resultaLista;
+ document.getElementById(lista-numeros).innerHTML = resultaLista;
 
 
  resultaLista = "";
  for(iteracion = 1; iteracion < 6; iteracion++){
-    resultaLista +=  `<td>`+iteracion+`</td>`
+    resultaLista +=  <td>+iteracion+</td>
  }
-     document.getElementById(`lista`).innerHTML = resultaLista;
-
- console.log("hola")
+     document.getElementById(lista).innerHTML = resultaLista;
+     */
