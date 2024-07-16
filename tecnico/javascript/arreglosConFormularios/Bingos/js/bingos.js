8e15596365a8cdae90f0ad47b0cc522eecabf3d7
SENA <SENA@NEIDFPSITFSP022.FormacionSena.RED>
2024-07-16 17:39:51 -0500

    clase de ingles

--- tecnico/javascript/arreglosConFormularios/Bingos/js/bingos.js
+++ tecnico/javascript/arreglosConFormularios/Bingos/js/bingos.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
     let contador = 0;
     let imprimir = '';
 
-   
     for (iteracion1 = 0; iteracion1 < 5; iteracion1++) {
         let interno = [];
         for (iteracion2 = 0; iteracion2 < 5; iteracion2++) {
@@ -21,11 +20,10 @@ document.addEventListener('DOMContentLoaded', function() {
             interno.push(tabla);
         }
         bingo.push(interno);
-    }
-   
-
+    };
     // Primera tabla de bingo simple
-    imprimir += '<table class="table" style="border: 1px solid #000; width: 300px; margin: 5px;">';
+    for(iteracion1 =0; iteracion1<1; iteracion1++){
+        imprimir += '<table class="table" style="border: 1px solid #000; width: 300px; margin: 5px;">';
 
     imprimir += '<tr>';
     for (let letra of letras) {
@@ -39,10 +37,10 @@ document.addEventListener('DOMContentLoaded', function() {
             imprimir += '<td>' + bingo[iteracion1][iteracion2] + '</td>';
         }
         imprimir += '</tr>';
-    }
+    };
 
     imprimir += '</table>';
-
+    };
     // letras 
     for (iteracion1 = 0; iteracion1 < 5; iteracion1++) {
         imprimir += '<table class="table" style ="border: 1px solid#000; width:300px; margin:5px;">';
@@ -50,10 +48,8 @@ document.addEventListener('DOMContentLoaded', function() {
     
         for (let contadorB = 0; contadorB < letras.length; contadorB++) {
             imprimir += '<th>' + letras[contadorB] + '</th>';
-        }
-    
+        };
         imprimir += '</tr>';
-    
         for (let contador = 0; contador < 5; contador++) {
             imprimir += '<tr>';
             for (iteracion2 = 0; iteracion2 < 5; iteracion2++) {
@@ -68,22 +64,17 @@ document.addEventListener('DOMContentLoaded', function() {
     
         imprimir += '</table>';
     };
-    
     // Tercera tabla de bingo con la X grande
-    
-    imprimir += '<table class="table" style ="border: 1px solid#000; width:300px; margin:5px;">'
+    for(iteracion1 =0; iteracion1<1; iteracion1++){
+        imprimir += '<table class="table" style ="border: 1px solid#000; width:300px; margin:5px;">'
     imprimir += '<tr>';
-    
     for (let contadorB = 0; contadorB < letras.length; contadorB++) {
         imprimir += '<th>' + letras[contadorB] + '</th>';
-    }
-
+    };
     imprimir += '</tr>';
-    for(iteracion1 =0; iteracion1 <5; iteracion1++){
-         
+    for(iteracion1 =0; iteracion1 <5; iteracion1++){    
         imprimir += '<tr>';
         for(iteracion2 = 0; iteracion2 < 5; iteracion2++){
-
             if(iteracion2 == iteracion1 ){
                 imprimir += '<td>'+'<strong>'+bingo[iteracion1][iteracion2]+'</strong>'+'</td>'
             }else if(iteracion2 == 5-(iteracion1+1)){
@@ -91,16 +82,11 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             else{
                 imprimir += '<td>'+bingo[iteracion1][iteracion2]+'</td>';
-            }
+            };
         };
         imprimir += '</tr>';
     };
     imprimir += '</table>';
-
-
-
-
-
-
+    };
     document.getElementById('bingo').innerHTML = imprimir;
 });
